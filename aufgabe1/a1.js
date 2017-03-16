6fa2173b3423c0d44257c019aef197654455a8a9
Dominik <Dominik@DominikUni.hfu-open.fu.hs-furtwangen.de>
2017-03-16 12:09:02 +0100

    commit

---
+++ aufgabe1/a1.js
@@ -0,0 +1,17 @@
+/*
+Aufgabe: Aufgabe 1
+Name: Braun Dominik
+Matrikel: 254901
+Datum: 15.03.2017
+Hiermit versichere ich, dass ich diesen
+Code selbst geschrieben habe. Er wurde
+nicht kopiert und auch nicht diktiert.
+*/
+document.addEventListener('DOMContentLoaded', function () {
+    var name;
+    name = prompt("Bitte gebe deinen Namen ein");
+    if (name != null) {
+        document.getElementsByTagName("div")[0].innerHTML = "Hallo " + name;
+    }
+});
+//# sourceMappingURL=a1.js.map
