79e3de618503472c779fb9d5a5cc07f69f69c645
Dominik <Dominik@activate.adobe.com>
2017-04-06 16:59:07 +0200

    a4.2 ablegen

--- aufgabe4.2/main.js
+++ aufgabe4.2/main.js
@@ -1,5 +1,6 @@
 window.onload = function () {
     let ziehstapel = ["Herz-Ass", "Karo-Ass", "Pik-Ass", "Kreuz-Ass", "Herz-K�nig", "Karo-K�nig", "Pik-K�nig", "Kreuz-K�nig", "Herz-Dame", "Karo-Dame", "Pik-Dame", "Kreuz-Dame", "Herz-Bube", "Karo-Bube", "Pik-Bube", "Kreuz-Bube", "Herz-10", "Karo-10", "Pik-10", "Kreuz-10", "Herz-9", "Karo-9", "Pik-9", "Kreuz-9", "Herz-8", "Karo-8", "Pik-8", "Kreuz-8", "Herz-7", "Karo-7", "Pik-7", "Kreuz-7"];
+    let hand = [];
     var ziehDiv = document.createElement("div");
     var ablageDiv = document.createElement("div");
     var handDiv = document.createElement("div");
@@ -8,7 +9,7 @@ window.onload = function () {
     //    var hand3Div: HTMLDivElement = document.createElement("div");
     //    var hand4Div: HTMLDivElement = document.createElement("div");
     //    var hand5Div: HTMLDivElement = document.createElement("div");
-    var handDivs = [document.createElement("div"), document.createElement("div"), document.createElement("div"), document.createElement("div"), document.createElement("div"), document.createElement("div")];
+    var handDivs = [document.createElement("div"), document.createElement("div"), document.createElement("div"), document.createElement("div"), document.createElement("div")];
     //    var ziehDiv: HTMLDivElement = div;
     //    var ablageDiv: HTMLDivElement = div;
     //    var handDiv: HTMLDivElement = div;
@@ -18,6 +19,7 @@ window.onload = function () {
     ziehDiv.innerHTML = "Ziehstapel";
     ziehDiv.style.backgroundColor = "black";
     ziehDiv.style.color = "white";
+    ablageDiv.innerHTML = "Ablagestapel";
     //    document.body.lastChild.appendChild(hand1Div);
     //    document.body.lastChild.appendChild(hand2Div);
     //    document.body.lastChild.appendChild(hand3Div);
@@ -26,10 +28,34 @@ window.onload = function () {
     handDiv.style.clear = "both";
     handDiv.style.height = "30vmin";
     handDiv.style.width = "80vmin";
-    for (var i = 1; i < handDivs.length; i++) {
+    for (var i = 0; i < handDivs.length; i++) {
         document.body.lastChild.appendChild(handDivs[i]);
         handDivs[i].style.width = "16vmin";
         handDivs[i].style.height = "30vmin";
+        handDivs[i].style.fontSize = "1vmin";
+        handDivs[i].addEventListener("click", ablegen);
+    }
+    ziehDiv.addEventListener("click", ziehen);
+    function ziehen() {
+        if (ziehstapel.length > 0) {
+            console.log(ziehstapel[0]);
+            let removed = ziehstapel.splice(0, 1);
+            if (hand.length < 5) {
+                hand.push(removed[0]);
+                console.log(hand[0], hand[1], hand[2], hand[3], hand[4]);
+            }
+        }
+        updateHand();
+    }
+    function ablegen() {
+        hand.splice(i, 1);
+        console.log(hand[0]);
+        updateHand();
+    }
+    function updateHand() {
+        for (var i = 0; i < handDivs.length; i++) {
+            handDivs[i].innerHTML = hand[i];
+        }
     }
     console.log(ziehstapel[0], ziehstapel[31]);
 };
--- aufgabe4.2/main.ts
+++ aufgabe4.2/main.ts
@@ -1,5 +1,6 @@
 window.onload = function(): void {
     let ziehstapel: string[] = ["Herz-Ass", "Karo-Ass", "Pik-Ass", "Kreuz-Ass", "Herz-König", "Karo-König", "Pik-König", "Kreuz-König", "Herz-Dame", "Karo-Dame", "Pik-Dame", "Kreuz-Dame", "Herz-Bube", "Karo-Bube", "Pik-Bube", "Kreuz-Bube", "Herz-10", "Karo-10", "Pik-10", "Kreuz-10", "Herz-9", "Karo-9", "Pik-9", "Kreuz-9", "Herz-8", "Karo-8", "Pik-8", "Kreuz-8", "Herz-7", "Karo-7", "Pik-7", "Kreuz-7"];
+    let hand: string[] = [];
 
     var ziehDiv: HTMLDivElement = document.createElement("div");
     var ablageDiv: HTMLDivElement = document.createElement("div");
@@ -9,7 +10,7 @@ window.onload = function(): void {
     //    var hand3Div: HTMLDivElement = document.createElement("div");
     //    var hand4Div: HTMLDivElement = document.createElement("div");
     //    var hand5Div: HTMLDivElement = document.createElement("div");
-    var handDivs: HTMLDivElement[] = [document.createElement("div"), document.createElement("div"), document.createElement("div"), document.createElement("div"), document.createElement("div"), document.createElement("div")]
+    var handDivs: HTMLDivElement[] = [document.createElement("div"), document.createElement("div"), document.createElement("div"), document.createElement("div"), document.createElement("div")]
     //    var ziehDiv: HTMLDivElement = div;
     //    var ablageDiv: HTMLDivElement = div;
     //    var handDiv: HTMLDivElement = div;
@@ -17,10 +18,12 @@ window.onload = function(): void {
     document.body.appendChild(ziehDiv);
     document.body.appendChild(ablageDiv);
     document.body.appendChild(handDiv);
-    
+
     ziehDiv.innerHTML = "Ziehstapel";
     ziehDiv.style.backgroundColor = "black";
     ziehDiv.style.color = "white";
+
+    ablageDiv.innerHTML = "Ablagestapel";
     //    document.body.lastChild.appendChild(hand1Div);
     //    document.body.lastChild.appendChild(hand2Div);
     //    document.body.lastChild.appendChild(hand3Div);
@@ -31,10 +34,39 @@ window.onload = function(): void {
     handDiv.style.height = "30vmin";
     handDiv.style.width = "80vmin";
 
-    for (var i: number = 1; i < handDivs.length; i++) {
+    for (var i: number = 0; i < handDivs.length; i++) {
         document.body.lastChild.appendChild(handDivs[i]);
         handDivs[i].style.width = "16vmin";
         handDivs[i].style.height = "30vmin";
+        handDivs[i].style.fontSize = "1vmin";
+        handDivs[i].addEventListener("click", ablegen);
+    }
+
+    ziehDiv.addEventListener("click", ziehen);
+
+    function ziehen(): void {
+        if (ziehstapel.length > 0) {
+            console.log(ziehstapel[0]);
+            let removed: string[] = ziehstapel.splice(0, 1);
+            if (hand.length < 5) {
+                hand.push(removed[0]);
+                console.log(hand[0], hand[1], hand[2], hand[3], hand[4]);
+            }
+        }
+        updateHand();
+    }
+
+    function ablegen(): void {
+        hand.splice(i, 1);
+        console.log(hand[0]);
+        updateHand();
+    }
+
+    function updateHand(): void {
+        for (var i: number = 0; i < handDivs.length; i++) {
+            handDivs[i].innerHTML = hand[i];
+
+        }
     }
 
     console.log(ziehstapel[0], ziehstapel[31]);
