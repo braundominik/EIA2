b4a9ae368579800c4402e0ed7d616e43319bc838
Dominik <Dominik@DominikPC.ghb.hs-furtwangen.de>
2017-04-12 23:24:35 +0100

    get heigth and width

---
+++ aufgabe5/main.ts
@@ -0,0 +1,15 @@
+/*
+Aufgabe: Aufgabe 3
+Name: Braun Dominik
+Matrikel: 254901
+Datum: 09.04.2017
+Hiermit versichere ich, dass ich diesen
+Code selbst geschrieben habe. Er wurde
+nicht kopiert und auch nicht diktiert.
+*/
+
+window.onload = function(): void {
+    var w: number = document.getElementsByTagName("HTML")[0].clientWidth;
+    var h: number = document.getElementsByTagName("HTML")[0].clientHeight;
+    document.body.innerHTML = "Width: " + w + " Height: " + h;
+};
