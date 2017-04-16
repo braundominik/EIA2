b0ee13f8286f63d2192a0c572fc80a49bf75f6ee
Dominik <Dominik@DominikUni>
2017-04-17 01:49:05 +0200

    flower1

--- aufgabe5/main.js
+++ aufgabe5/main.js
@@ -14,12 +14,33 @@ var a5;
     function init() {
         let canvas;
         canvas = document.createElement("canvas");
-        canvas.height = 1400;
-        canvas.width = 900;
+        canvas.height = 450;
+        canvas.width = 700;
         document.body.appendChild(canvas);
         crc = canvas.getContext("2d");
-        crc.fillStyle = "#0000ff";
+        crc.fillStyle = "#a9c5f2";
         crc.fillRect(0, 0, canvas.width, canvas.height);
+        flower1(200, 200);
+    }
+    function flower1(x, y) {
+        crc.fillStyle = "red";
+        crc.fillRect(5, 5, 5, -5);
+        crc.beginPath();
+        crc.fillStyle = "green";
+        crc.fillRect(x, y, 4, -40);
+        crc.ellipse(x + 15, y - 17, 4, 18, 45 * Math.PI / 180, 0, 2 * Math.PI);
+        crc.ellipse(x - 10, y - 20, 4, 18, -45 * Math.PI / 180, 0, 2 * Math.PI);
+        crc.fill();
+        crc.beginPath();
+        crc.fillStyle = "white";
+        for (let i = 0; i < 6; i++) {
+            crc.ellipse(x + 2, y - 45, 3, 15, (i * 30) * Math.PI / 180, 0, 2 * Math.PI);
+        }
+        crc.fill();
+        crc.beginPath();
+        crc.fillStyle = "yellow";
+        crc.arc(x + 2, y - 45, 5, 0, 2 * Math.PI);
+        crc.fill();
     }
 })(a5 || (a5 = {}));
 //# sourceMappingURL=main.js.map
--- aufgabe5/main.ts
+++ aufgabe5/main.ts
@@ -11,16 +11,44 @@ nicht kopiert und auch nicht diktiert.
 namespace a5 {
     window.addEventListener("load", init);
     let crc: CanvasRenderingContext2D;
-    
+
     function init(): void {
         let canvas: HTMLCanvasElement;
         canvas = document.createElement("canvas");
-        canvas.height = 1400;
-        canvas.width = 900;
+        canvas.height = 450;
+        canvas.width = 700;
         document.body.appendChild(canvas);
         crc = canvas.getContext("2d");
-        
-        crc.fillStyle = "#0000ff";
-        crc.fillRect(0,0,canvas.width,canvas.height);
+
+        crc.fillStyle = "#a9c5f2";
+        crc.fillRect(0, 0, canvas.width, canvas.height);
+        flower1(200, 200);
+    }
+
+    function flower1(x: number, y: number): void {
+          crc.fillStyle = "red";
+        crc.fillRect(5,5,5,-5);
+
+        crc.beginPath();
+        crc.fillStyle = "green";
+        crc.fillRect(x, y, 4, -40);
+        crc.ellipse(x + 15, y - 17, 4, 18, 45 * Math.PI / 180, 0, 2 * Math.PI);
+        crc.ellipse(x - 10, y - 20, 4, 18, -45 * Math.PI / 180, 0, 2 * Math.PI);
+        crc.fill();
+
+        crc.beginPath();
+        crc.fillStyle = "white";
+        for (let i: number = 0; i < 6; i++) {
+            crc.ellipse(x + 2, y - 45, 3, 15, (i * 30) * Math.PI / 180, 0, 2 * Math.PI);
+        }
+        crc.fill();
+
+
+        crc.beginPath();
+        crc.fillStyle = "yellow";
+        crc.arc(x + 2, y - 45, 5, 0, 2 * Math.PI);
+
+        crc.fill();
+
     }
 }  
