5f10a8d3f36969b554d8d2bd2e3f173932dadd6c
Dominik <Dominik@DominikUni.hfu-open.fu.hs-furtwangen.de>
2017-04-20 10:34:52 +0200

    flower bereich

--- aufgabe5/main.js
+++ aufgabe5/main.js
@@ -20,8 +20,12 @@ var a5;
         crc = canvas.getContext("2d");
         crc.fillStyle = "#a9c5f2";
         crc.fillRect(0, 0, canvas.width, canvas.height);
-        flower3(200, 200);
-        flower2(300, 300);
+        crc.fillStyle = "#32722c";
+        crc.fillRect(400, 200, 300, 250);
+        //        flower3(200, 200);
+        //        flower2(300, 300);
+        //        rwer(400, 400);
+        placeFlowersIn(400, 200, 700, 450);
     }
     function flower1(x, y) {
         crc.beginPath();
@@ -43,17 +47,17 @@ var a5;
     }
     function flower2(x, y) {
         crc.beginPath();
-        crc.fillStyle = "green";
+        crc.fillStyle = "rgba(53, 195, 40, 0.5)";
         crc.fillRect(x, y, 4, -40);
         crc.ellipse(x + 15, y - 17, 4, 18, 45 * Math.PI / 180, 0, 2 * Math.PI);
         crc.ellipse(x - 10, y - 20, 4, 18, -45 * Math.PI / 180, 0, 2 * Math.PI);
         crc.fill();
         crc.beginPath();
-        crc.fillStyle = "red";
+        crc.fillStyle = "rgba(219, 0, 7,0.8)";
         crc.ellipse(x + 2, y - 45, 11, 13, 1 * Math.PI / 180, 0, 1 * Math.PI);
         crc.fill();
         crc.beginPath();
-        crc.fillStyle = "red";
+        crc.fillStyle = "rgba(219, 0, 7,0.8)";
         crc.moveTo(x - 9, y - 50 + 5);
         crc.lineTo(x - 9, y - 50 - 5);
         crc.lineTo(x - 1, y - 50 + 5);
@@ -77,13 +81,13 @@ var a5;
         let rnd2 = Math.random() * (18 - 12) + 12;
         let rndColor = Math.random() * 359;
         crc.beginPath();
-        crc.fillStyle = "green";
+        crc.fillStyle = "rgba(53, 195, 40, 0.5)";
         crc.fillRect(x, y, 4, -40);
         crc.ellipse(x + 15, y - 17, 4, 18, 45 * Math.PI / 180, 0, 2 * Math.PI);
         crc.ellipse(x - 10, y - 20, 4, 18, -45 * Math.PI / 180, 0, 2 * Math.PI);
         crc.fill();
         crc.beginPath();
-        crc.fillStyle = "hsl(" + rndColor + ", 100%, 76%)";
+        crc.fillStyle = "hsla(" + rndColor + ", 100%, 50%,0.8)";
         for (let i = 0; i < 6; i++) {
             crc.ellipse(x + 2, y - 45, rnd1, rnd2, (i * 30) * Math.PI / 180, 0, 2 * Math.PI);
         }
@@ -92,6 +96,22 @@ var a5;
         crc.fillStyle = "yellow";
         crc.arc(x + 2, y - 45, 5, 0, 2 * Math.PI);
         crc.fill();
+    }
+    function randomFlower(x, y) {
+        let rnd = Math.random();
+        if (rnd < 0.7) {
+            flower3(x, y);
+        }
+        else {
+            flower2(x, y);
+        }
+    }
+    function placeFlowersIn(upperLeftx, upperLefty, lowerRightx, lowerRighty) {
+        for (let f = 0; f < 1000; f++) {
+            let rndX = Math.random() * (lowerRightx - upperLeftx) + upperLeftx;
+            let rndY = Math.random() * (lowerRighty - upperLefty) + upperLefty;
+            randomFlower(rndX, rndY);
+        }
     }
 })(a5 || (a5 = {}));
 //# sourceMappingURL=main.js.map
--- aufgabe5/main.ts
+++ aufgabe5/main.ts
@@ -22,8 +22,16 @@ namespace a5 {
 
         crc.fillStyle = "#a9c5f2";
         crc.fillRect(0, 0, canvas.width, canvas.height);
-        flower3(200, 200);
-        flower2(300, 300);
+
+        crc.fillStyle = "#32722c";
+        crc.fillRect(400, 200, 300, 250);
+
+        //        flower3(200, 200);
+        //        flower2(300, 300);
+        //        rwer(400, 400);
+
+        placeFlowersIn(400, 200, 700, 450);
+
     }
 
     function flower1(x: number, y: number): void {
@@ -53,19 +61,19 @@ namespace a5 {
 
     function flower2(x: number, y: number): void {
         crc.beginPath();
-        crc.fillStyle = "green";
+        crc.fillStyle = "rgba(53, 195, 40, 0.5)";
         crc.fillRect(x, y, 4, -40);
         crc.ellipse(x + 15, y - 17, 4, 18, 45 * Math.PI / 180, 0, 2 * Math.PI);
         crc.ellipse(x - 10, y - 20, 4, 18, -45 * Math.PI / 180, 0, 2 * Math.PI);
         crc.fill();
 
         crc.beginPath();
-        crc.fillStyle = "red";
+        crc.fillStyle = "rgba(219, 0, 7,0.8)";
         crc.ellipse(x + 2, y - 45, 11, 13, 1 * Math.PI / 180, 0, 1 * Math.PI);
         crc.fill();
 
         crc.beginPath();
-        crc.fillStyle = "red";
+        crc.fillStyle = "rgba(219, 0, 7,0.8)";
         crc.moveTo(x - 9, y - 50 + 5);
         crc.lineTo(x - 9, y - 50 - 5);
         crc.lineTo(x - 1, y - 50 + 5);
@@ -95,14 +103,14 @@ namespace a5 {
 
 
         crc.beginPath();
-        crc.fillStyle = "green";
+        crc.fillStyle = "rgba(53, 195, 40, 0.5)";
         crc.fillRect(x, y, 4, -40);
         crc.ellipse(x + 15, y - 17, 4, 18, 45 * Math.PI / 180, 0, 2 * Math.PI);
         crc.ellipse(x - 10, y - 20, 4, 18, -45 * Math.PI / 180, 0, 2 * Math.PI);
         crc.fill();
 
         crc.beginPath();
-        crc.fillStyle = "hsl(" + rndColor + ", 100%, 76%)";
+        crc.fillStyle = "hsla(" + rndColor + ", 100%, 50%,0.8)";
         for (let i: number = 0; i < 6; i++) {
             crc.ellipse(x + 2, y - 45, rnd1, rnd2, (i * 30) * Math.PI / 180, 0, 2 * Math.PI);
         }
@@ -114,6 +122,26 @@ namespace a5 {
         crc.arc(x + 2, y - 45, 5, 0, 2 * Math.PI);
 
         crc.fill();
+    }
+
+    function randomFlower(x: number, y: number): void {
+        let rnd: number = Math.random();
+        if (rnd < 0.7) {
+            flower3(x, y);
+        }
+        else {
+            flower2(x, y);
+        }
+
+    }
+
+    function placeFlowersIn(upperLeftx: number, upperLefty: number, lowerRightx: number, lowerRighty: number): void {
+        for (let f: number = 0; f < 1000; f++) {
+            let rndX: number = Math.random() * (lowerRightx - upperLeftx) + upperLeftx;
+            let rndY: number = Math.random() * (lowerRighty - upperLefty) + upperLefty;
+            randomFlower(rndX, rndY);
+        }
+
     }
 
 }  
