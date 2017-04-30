0047f575c495b11a41c3ca91e10ee1bf523fa6a5
Dominik <Dominik@DominikPC>
2017-04-30 11:28:53 +0100

    Steckbrief Update + minor changes

--- aufgabe5/bee.js
+++ aufgabe5/bee.js
@@ -1,1 +1,10 @@
+/*
+Aufgabe: Aufgabe 5
+Name: Braun Dominik
+Matrikel: 254901
+Datum: 30.04.2017
+Hiermit versichere ich, dass ich diesen
+Code selbst geschrieben habe. Er wurde
+nicht kopiert und auch nicht diktiert.
+*/
 //# sourceMappingURL=bee.js.map
--- aufgabe5/main.js
+++ aufgabe5/main.js
@@ -1,8 +1,8 @@
 /*
-Aufgabe: Aufgabe 3
+Aufgabe: Aufgabe 5
 Name: Braun Dominik
 Matrikel: 254901
-Datum: 09.04.2017
+Datum: 30.04.2017
 Hiermit versichere ich, dass ich diesen
 Code selbst geschrieben habe. Er wurde
 nicht kopiert und auch nicht diktiert.
